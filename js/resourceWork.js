7a492d6794efbeb9a31bc6812673d54fff8c9f5e
user-2495\user <A.Sashuk@newline.tech>
2016-11-24 13:14:05 +0200

    fix quot

--- js/resourceWork.js
+++ js/resourceWork.js
@@ -208,7 +208,7 @@ function failDownload(error) {
 }
 
 function fail(error) {
-  console.log("/----ERROR-------/")
+  console.log("/----ERROR-------/");
     console.log(error.code);
     console.log(error.http_status);
     console.log(error.body);
