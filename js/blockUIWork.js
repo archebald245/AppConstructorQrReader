acf69a92165efcaa28d6519f6693f5a749907177
user-2495\user <A.Sashuk@newline.tech>
2017-03-23 13:28:10 +0200

    add fix for backround image and external link

--- js/blockUIWork.js
+++ js/blockUIWork.js
@@ -1,29 +1,34 @@
-function blockUi(){
-     $.blockUI({ css: {
-         border: 'none',
-         padding: '15px',
-         backgroundColor: '#000',
-         '-webkit-border-radius': '10px',
-         '-moz-border-radius': '10px',
-         opacity: .5,
-         color: '#fff'
-    } });
-
+function blockUi() {
+    $.blockUI({
+        css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        }
+    });
 }
 
-function unBlockUi(){
+function unBlockUi() {
     createCustomHideForms();
     bindChangeValForms();
     addListenerToClickBuy();
     addListenerToClickOpenSingleItem();
     bindListenerToClickBtn();
     addListenerToClickTimeLine();
-    var pageStyles = applicationData.Pages[0].Style;
-    if (pageStyles != undefined) {
-        $("#container").attr("style", pageStyles);
+    var pageStyles;
+    var pageWithGeneralBg = applicationData.Pages.filter(function(page) { return page.BackgroundForApplication });
+    if (pageWithGeneralBg.length > 0) {
+        pageStyles = pageWithGeneralBg[0].Style;
     }
+    if (applicationData.Pages[0].Style != null) {
+        pageStyles = applicationData.Pages[0].Style;
+    }
+    $("#container").attr("style", pageStyles);
     changeRestaurant();
     changeMenu();
     $.unblockUI();
-
 }
