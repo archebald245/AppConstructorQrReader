70cfbc6ba69767c319c886e1629e399f79639585
user-2495\user <A.Sashuk@newline.tech>
2017-03-23 15:03:07 +0200

    add tracking last page for background

--- js/initWork.js
+++ js/initWork.js
@@ -81,10 +81,19 @@ function onCheckJson() {
         if (pageWithGeneralBg.length > 0) {
             pageStyles = pageWithGeneralBg[0].Style;
         }
-        var StartPage = applicationData.Pages.filter(function(p){return p.IsStartPage})[0];
-        if (StartPage.BackgroundImagePath != null) {
-            pageStyles = StartPage.Style;
-        }   
+        if(applicationData.IsTrackingLastPage){
+            var idLastPage = getLastOpenPage();
+            var lastPage = applicationData.Pages.filter(function(p){return p.Id = idLastPage})[0]
+            if (lastPage.BackgroundImagePath != null) {
+                pageStyles = lastPage.Style;
+            }
+        }else{
+            var StartPage = applicationData.Pages.filter(function(p){return p.IsStartPage})[0];
+            if (StartPage.BackgroundImagePath != null) {
+                pageStyles = StartPage.Style;
+            }
+        }
+   
         $("#container").attr("style", pageStyles);
     } else {
 
