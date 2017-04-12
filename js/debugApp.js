3edab2572e06028454d70ca75f8a21d987d941f9
user-2495\user <A.Sashuk@newline.tech>
2017-04-12 17:14:33 +0300

    change config

--- js/debugApp.js
+++ js/debugApp.js
@@ -14,7 +14,7 @@ function startScan() { //Call in Init function
 
     cordova.plugins.barcodeScanner.scan(
         function(result) {
-            var siteUrl = "http://appconstructor.newline.tech";
+            var siteUrl = "http://appconstructornew.newlinetechnologies.net";
             if (!result.cancelled) {
                 $(".Scan-spiner").removeClass("hidden");
                 var ProjectId = result.text.split("-")[0];
