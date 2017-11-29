09154b216dc6563c56f2b7bf16cc11bb02051b38
yevhen pugachov <y.pugachov@newline.tech>
2017-11-29 17:53:43 +0200

    fix

--- js/render-project-list.js
+++ js/render-project-list.js
@@ -47,7 +47,7 @@ function renderProjectList(projectList) {
                         ),
                         React.createElement(
                             "button", { className: "take-application" },
-                            cultureRes.download
+                            "Download"
                         )
                     ),
                     React.createElement("input", { type: "hidden", name: "projectId", value: item.ProjectId })
