ae03462166f930189e581352fb1113b2c4231427
yevhen pugachov <y.pugachov@newline.tech>
2018-02-07 13:40:32 +0200

    fix display login form with keyboard and update pdf work logic

--- js/MobileView.js
+++ js/MobileView.js
@@ -865,18 +865,43 @@ function reactRender() {
             if (data.ContentTypeId == 17 && this.checkDeniedTools(deniedTools, "pdf-item")) {
                 $(ReactDOM.findDOMNode(this)).find("span").click(function(e) {
                     var url = $(this).attr("data-locationpdf");
-                    var options = {
-                        openWith: {
-                            enabled: true
-                        }
-                    }
 
-                    function onShow() {
-                        window.console.log('document shown');
-                        //e.g. track document usage
+                    if (device.platform === 'iOS') {
+                        //ios
+                        var options = {
+                            openWith: {
+                                enabled: true
+                            }
+                        }
+
+                        function onShow() {
+                            window.console.log('document shown');
+                            //e.g. track document usage
+                        }
+                        cordova.plugins.SitewaertsDocumentViewer.viewDocument(
+                            url, 'application/pdf', options, onShow);
+
+                    } else {
+                        //android
+                        window.resolveLocalFileSystemURL(url, function(fileEntry) {
+                            window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dirEntry) {
+                                fileEntry.copyTo(dirEntry, 'file.pdf', function(newFileEntry) {
+                                    cordova.plugins.fileOpener2.open(newFileEntry.nativeURL, 'application/pdf', {
+                                        error: function(e) {
+                                            if (e.message.indexOf("Activity not found: No Activity found to handle Intent") > -1) {
+                                                window.plugins.toast.showShortBottom("Please, install some PDF reader.");
+                                            }
+                                            console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+                                        },
+                                        success: function() {
+                                            console.log('file opened successfully');
+                                        }
+                                    });
+                                });
+                            });
+                        });
+
                     }
-                    cordova.plugins.SitewaertsDocumentViewer.viewDocument(
-                        url, 'application/pdf', options, onShow);
                 });
             }
             if (data.ContentTypeId == 19 && this.checkDeniedTools(deniedTools, "event-item")) {
