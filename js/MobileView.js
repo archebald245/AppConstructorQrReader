09045de50be736731e9b1cfb26807ef46bd836e0
yevhen pugachov <y.pugachov@newline.tech>
2018-02-09 11:31:11 +0200

    add notification

--- js/MobileView.js
+++ js/MobileView.js
@@ -683,6 +683,9 @@ function reactRender() {
                         $("#form-container").attr("id", "");
                         if ($.jStorage.get('isLogin') && element.RegistrationForm) {
                             $(".form-container").find('input, button, textarea').prop("disabled", true);
+                            $(".form-container").on("click", function() {
+                                window.plugins.toast.showShortBottom("LogOut before");
+                            });
                         }
                         if (data.CountFormColumn == 2) {
                             $(".formBlock").addClass("formHalf");
